027f050bf426b9b453b4fde5a322a590b1321cdc
Dan Roberts <danroberts728@gmail.com>
2019-10-31 14:01:54 -0500

    Update splash screen

--- android/app/src/main/java/org/alabamabrewers/albeertrail/MainActivity.java
+++ android/app/src/main/java/org/alabamabrewers/albeertrail/MainActivity.java
@@ -1,6 +1,8 @@
 package org.alabamabrewers.albeertrail;
 
 import android.os.Bundle;
+import android.view.ViewTreeObserver;
+import android.view.WindowManager;
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
@@ -9,5 +11,14 @@ public class MainActivity extends FlutterActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     GeneratedPluginRegistrant.registerWith(this);
+    //Remove full screen flag after load
+    ViewTreeObserver vto = getFlutterView().getViewTreeObserver();
+    vto.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+      @Override
+      public void onGlobalLayout() {
+        getFlutterView().getViewTreeObserver().removeOnGlobalLayoutListener(this);
+        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+      }
+    });
   }
 }
