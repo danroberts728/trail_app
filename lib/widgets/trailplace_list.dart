bd7978b5f14319cefa1d561aa1655d428c109410
Dan Roberts <danroberts728@gmail.com>
2019-11-08 15:35:01 -0600

    No need to refresh page if sort order is the same

--- lib/widgets/trailplace_list.dart
+++ lib/widgets/trailplace_list.dart
@@ -27,8 +27,7 @@ class TrailListViewState extends State<TrailListView> {
   final _searchController = new TextEditingController();
   bool _showSearchBar = false;
   FilterOptions _filter;
-
-  Widget _trailList = Center(child: CircularProgressIndicator());
+  bool _showUpdate;
 
   var _locationBloc = LocationBloc();
 
@@ -38,7 +37,7 @@ class TrailListViewState extends State<TrailListView> {
 
   void showSearchBar() {
     setState(() {
-      this._showSearchBar = true;
+      _showSearchBar = true;
     });
   }
 
@@ -50,22 +49,30 @@ class TrailListViewState extends State<TrailListView> {
         return ModalTrailFilter(initialOptions: this._filter);
       },
     ).then((value) {
-      this._filter = value;
-      _refreshScreen();
+      bool doUpdate = false;
+      if (this._filter.sort != value.sort) {
+        doUpdate = true;
+      }
+      setState(() {
+        this._filter = value;
+      });      
+      if (doUpdate) {
+        _refreshScreen();
+      }
     });
   }
 
   void _refreshScreen() {
     // Clear the screen
     setState(() {
-      _trailList = Center(child: CircularProgressIndicator());
+      _showUpdate = true;
     });
     // Sort the places
     sortPlaces();
     // Return the screen
     Future.delayed(const Duration(seconds: 1), () {
       setState(() {
-        _trailList = buidTrailList();
+        _showUpdate = false;
       });
     });
   }
@@ -98,107 +105,105 @@ class TrailListViewState extends State<TrailListView> {
     this._filter = FilterOptions(SortOrder.DISTANCE, initialShow);
 
     sortPlaces();
-    _trailList = buidTrailList();
+    _showUpdate = false;
 
     _locationBloc.locationStream.listen(_onLocationUpdate);
   }
 
   @override
   Widget build(BuildContext context) {
-    return _trailList;
-  }
-
-  Widget buidTrailList() {
-    return RefreshIndicator(
-      onRefresh: () {
-        setState(() {
-          this._trailList = Center(child: CircularProgressIndicator());
-        });
-        return _locationBloc.refreshLocation();
-      },
-      child: Container(
-        child: Column(
-          children: <Widget>[
-            // Search
-            AnimatedContainer(
-              duration: Duration(milliseconds: 500),
-              height: _showSearchBar ? 50.0 : 0.0,
-              padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
-              child: Visibility(
-                visible: _showSearchBar,
-                child: TextField(
-                  onChanged: (query) {
-                    setState(() {
-                      _searchQuery = query;
-                    });
-                  },
-                  controller: _searchController,
-                  decoration: InputDecoration(
-                    contentPadding: EdgeInsets.symmetric(vertical: 0.0),
-                    suffixIcon: _showSearchBar
-                        ? IconButton(
-                            icon: Icon(Icons.clear, color: Colors.grey),
-                            onPressed: () {
-                              _searchController.clear();
-                              setState(() {
-                                FocusScope.of(context)
-                                    .requestFocus(FocusNode());
-                                _showSearchBar = false;
-                                _searchQuery = "";
-                              });
-                            },
-                          )
-                        : null,
-                    isDense: true,
-                    labelText: '',
-                    hintText: '',
-                    prefixIcon: Icon(Icons.search),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(8.0)),
+    if (_showUpdate) {
+      return Center(child: Center(child: CircularProgressIndicator()));
+    } else {
+      return RefreshIndicator(
+        onRefresh: () {
+          _showUpdate = true;
+          return _locationBloc.refreshLocation();
+        },
+        child: Container(
+          child: Column(
+            children: <Widget>[
+              // Search
+              AnimatedContainer(
+                duration: Duration(milliseconds: 500),
+                height: _showSearchBar ? 50.0 : 0.0,
+                padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
+                child: Visibility(
+                  visible: _showSearchBar,
+                  child: TextField(
+                    onChanged: (query) {
+                      setState(() {
+                        _searchQuery = query;
+                      });
+                    },
+                    controller: _searchController,
+                    decoration: InputDecoration(
+                      contentPadding: EdgeInsets.symmetric(vertical: 0.0),
+                      suffixIcon: _showSearchBar
+                          ? IconButton(
+                              icon: Icon(Icons.clear, color: Colors.grey),
+                              onPressed: () {
+                                _searchController.clear();
+                                setState(() {
+                                  FocusScope.of(context)
+                                      .requestFocus(FocusNode());
+                                  _showSearchBar = false;
+                                  _searchQuery = "";
+                                });
+                              },
+                            )
+                          : null,
+                      isDense: true,
+                      labelText: '',
+                      hintText: '',
+                      prefixIcon: Icon(Icons.search),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-            // ListView
-            Expanded(
-              child: Container(
-                padding: EdgeInsets.symmetric(vertical: 0, horizontal: 10.0),
-                child: ListView.builder(
-                  physics: const AlwaysScrollableScrollPhysics(),
-                  itemCount: widget.places.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    List<String> filterIncludes = List<String>();
-                    _filter.show.forEach((cat, show) {
-                      if (show) {
-                        filterIncludes.add(cat.name);
-                      }
-                    });
+              // ListView
+              Expanded(
+                child: Container(
+                  padding: EdgeInsets.symmetric(vertical: 0, horizontal: 10.0),
+                  child: ListView.builder(
+                    physics: const AlwaysScrollableScrollPhysics(),
+                    itemCount: widget.places.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      List<String> filterIncludes = List<String>();
+                      _filter.show.forEach((cat, show) {
+                        if (show) {
+                          filterIncludes.add(cat.name);
+                        }
+                      });
 
-                    if (!widget.places[index].categories
-                        .any((f) => filterIncludes.contains(f))) {
-                      // Remove items that don't match filter
-                      return Container();
-                    } else if (_searchQuery == null ||
-                        _searchQuery.isEmpty ||
-                        widget.places[index].name
-                            .toLowerCase()
-                            .contains(_searchQuery.toLowerCase())) {
-                      // Only include items that match search (or all filtered items if search is not active)
-                      return TrailListCard(
-                        place: widget.places[index],
-                      );
-                    } else {
-                      // Remove items that don't match search
-                      return Container();
-                    }
-                  },
+                      if (!widget.places[index].categories
+                          .any((f) => filterIncludes.contains(f))) {
+                        // Remove items that don't match filter
+                        return Container();
+                      } else if (_searchQuery == null ||
+                          _searchQuery.isEmpty ||
+                          widget.places[index].name
+                              .toLowerCase()
+                              .contains(_searchQuery.toLowerCase())) {
+                        // Only include items that match search (or all filtered items if search is not active)
+                        return TrailListCard(
+                          place: widget.places[index],
+                        );
+                      } else {
+                        // Remove items that don't match search
+                        return Container();
+                      }
+                    },
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
-      ),
-    );
+      );
+    }
   }
 }
