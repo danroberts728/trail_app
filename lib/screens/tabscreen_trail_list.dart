ccddbd3fd95454f11675c9ab85d62c9896ca3046
Dan Roberts <danroberts728@gmail.com>
2019-11-25 09:23:18 -0600

    Undo "Revert "Added Trail Map""
    This reverts commit 5e434d37dafc3668bf0deb0700dfaceb078a2cc3.

---
+++ lib/screens/tabscreen_trail_list.dart
@@ -0,0 +1,43 @@
+import 'package:alabama_beer_trail/blocs/trail_places_bloc.dart';
+import 'package:alabama_beer_trail/widgets/trailplace_list.dart';
+
+import 'package:flutter/material.dart';
+import '../data/trail_place.dart';
+
+/// The trail list screen
+///
+/// This is a sub-screen for the Trail Tab
+class TabScreenTrailList extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _TabScreenTrailList();
+}
+
+/// The state of the trail list screen
+///
+class _TabScreenTrailList extends State<TabScreenTrailList>
+    with AutomaticKeepAliveClientMixin<TabScreenTrailList> {
+  /// The global key for the list view state
+  var _trailListViewKey = GlobalKey<TrailListViewState>();
+
+  /// The BloC for the trail places
+  var _trailPlacesBloc = TrailPlacesBloc();
+
+  @override
+  Widget build(BuildContext context) {
+    super.build(context);
+    return StreamBuilder(
+      stream: _trailPlacesBloc.trailPlaceStream,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        } else {
+          List<TrailPlace> places = snapshot.data;
+          return TrailListView(key: _trailListViewKey, places: places);
+        }
+      },
+    );
+  }
+
+  @override
+  bool get wantKeepAlive => true;
+}
