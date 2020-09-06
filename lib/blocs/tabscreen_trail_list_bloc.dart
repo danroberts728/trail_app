de89829b8cd183a50f8c1ec18aec8f6f91d68db4
Dan Roberts <danroberts728@gmail.com>
2020-09-06 15:22:28 -0500

    Show list sorted on open

--- lib/blocs/tabscreen_trail_list_bloc.dart
+++ lib/blocs/tabscreen_trail_list_bloc.dart
@@ -1,7 +1,13 @@
 import 'dart:async';
+import 'dart:math';
 
 import 'package:alabama_beer_trail/data/trail_database.dart';
 import 'package:alabama_beer_trail/data/trail_place.dart';
+import 'package:alabama_beer_trail/data/trail_place_category.dart';
+import 'package:alabama_beer_trail/util/filter_options.dart';
+import 'package:alabama_beer_trail/util/geo_methods.dart';
+import 'package:alabama_beer_trail/util/location_service.dart';
+import 'package:alabama_beer_trail/util/trail_app_settings.dart';
 
 import 'bloc.dart';
 
@@ -9,26 +15,81 @@ import 'bloc.dart';
 class TabScreenTrailListBloc extends Bloc {
   final TrailDatabase _db = TrailDatabase();
   StreamSubscription _placesSubscription;
+  FilterOptions _filterOptions;
+  LocationService _locationService = LocationService();
 
-  List<TrailPlace> trailPlaces = List<TrailPlace>();
+  List<TrailPlace> allTrailPlaces = List<TrailPlace>();
 
-  final _placesStreamController = StreamController<List<TrailPlace>>();
-  Stream<List<TrailPlace>> get trailPlaceStream =>
-      _placesStreamController.stream;
+  List<TrailPlace> get filteredTrailPlaces {
+    // Filter
+    List<String> shownCategories = _filterOptions.show.keys
+        .where((f) => _filterOptions.show[f])
+        .map((e) => e.name)
+        .toList();
+    List<TrailPlace> filtered = allTrailPlaces
+        .where((p) => p.categories.any((c) => shownCategories.contains(c)))
+        .toList();
+    // Sort
+    if (_filterOptions.sort == SortOrder.DISTANCE &&
+        _locationService.lastLocation != null) {
+      return filtered
+        ..sort((a, b) {
+          return GeoMethods.calculateDistance(
+                  a.location, _locationService.lastLocation)
+              .compareTo(GeoMethods.calculateDistance(
+                  b.location, _locationService.lastLocation));
+        });
+    } else {
+      return filtered
+        ..sort((a, b) => a.name.toLowerCase().compareTo(b.name.toLowerCase()));
+    }
+  }
+
+  final _allPlacesStreamController = StreamController<List<TrailPlace>>();
+  Stream<List<TrailPlace>> get allTrailPlaceStream =>
+      _allPlacesStreamController.stream;
+
+  final _filteredPlacesStreamController = StreamController<List<TrailPlace>>();
+  Stream<List<TrailPlace>> get filteredTraiilPlacesStream =>
+      _filteredPlacesStreamController.stream;
 
   TabScreenTrailListBloc() {
-    trailPlaces = _db.places;
+    _locationService.locationStream.listen(_onLocationUpdate);
+    allTrailPlaces = _db.places;
+    var showAll = Map<TrailPlaceCategory, bool>();
+    TrailAppSettings.filterStrings.forEach((f) => showAll[f] = true);
+    _filterOptions = FilterOptions(
+        LocationService().lastLocation == null
+            ? SortOrder.ALPHABETICAL
+            : SortOrder.DISTANCE,
+        showAll);
     _placesSubscription = _db.placesStream.listen(_onPlacesUpdate);
   }
 
+  void changeFilter(FilterOptions filter) {
+    _filterOptions = filter;
+    _filteredPlacesStreamController.sink.add(filteredTrailPlaces);
+  }
+
+  Future<void> refreshPulled() {
+    return Future.delayed(Duration(seconds: 1))
+        .then((result) => _locationService.refreshLocation());
+  }
+
   void _onPlacesUpdate(List<TrailPlace> places) {
-    trailPlaces = places;
-    _placesStreamController.sink.add(places);
+    allTrailPlaces = places;
+    _allPlacesStreamController.sink.add(places);
+    _filteredPlacesStreamController.sink.add(filteredTrailPlaces);
+  }
+
+  void _onLocationUpdate(Point newLocation) {
+    _filteredPlacesStreamController.sink.add(filteredTrailPlaces);
   }
 
   @override
   void dispose() {
     _placesSubscription.cancel();
-    _placesStreamController.close();
+    _allPlacesStreamController.close();
+    _filteredPlacesStreamController.close();
   }
 }
--- lib/screens/tabscreen_trail_list.dart
+++ lib/screens/tabscreen_trail_list.dart
@@ -1,9 +1,9 @@
 import 'package:alabama_beer_trail/blocs/tabscreen_trail_list_bloc.dart';
-import 'package:alabama_beer_trail/widgets/trailplace_list.dart';
+import 'package:alabama_beer_trail/data/trail_place.dart';
+import 'package:alabama_beer_trail/widgets/trailplace_card.dart';
 import 'package:alabama_beer_trail/util/tabselection_service.dart';
 
 import 'package:flutter/material.dart';
-import '../data/trail_place.dart';
 
 /// The trail list screen
 /// This is a sub-screen for the Trail Tab
@@ -16,19 +16,14 @@ class TabScreenTrailList extends StatefulWidget {
 class _TabScreenTrailList extends State<TabScreenTrailList>
     with AutomaticKeepAliveClientMixin<TabScreenTrailList> {
   _TabScreenTrailList() {
-    _tabSelectionBloc.tabSelectionStream.listen((newTab) {
-      // Scroll to top if on this tab an tab tapped.
-      if (newTab == 0 && _tabSelectionBloc.lastTapSame) {
-        _trailListViewKey.currentState.scrollToTop();
-      }
-    });
+    _tabSelectionService.tabSelectionStream.listen(_scrollToTop);
   }
 
-  /// The global key for the list view state
-  var _trailListViewKey = GlobalKey<TrailListViewState>();
+  /// The ListView Controller
+  final _listViewController = ScrollController();
 
   /// The BLoC for the app tab selection
-  final _tabSelectionBloc = TabSelectionService();
+  final _tabSelectionService = TabSelectionService();
 
   /// The BloC for the trail places
   var _bloc = TabScreenTrailListBloc();
@@ -37,13 +32,42 @@ class _TabScreenTrailList extends State<TabScreenTrailList>
   Widget build(BuildContext context) {
     super.build(context);
     return StreamBuilder(
-      stream: _bloc.trailPlaceStream,
-      initialData: _bloc.trailPlaces,
+      stream: _bloc.filteredTraiilPlacesStream,
+      initialData: _bloc.filteredTrailPlaces,
       builder: (context, snapshot) {
         List<TrailPlace> places = snapshot.data;
-        return TrailListView(key: _trailListViewKey, places: places);
+        return RefreshIndicator(
+          onRefresh: _bloc.refreshPulled,
+          child: Container(
+            color: Colors.black12,
+            child: Container(
+              margin: EdgeInsets.all(0),
+              padding: EdgeInsets.all(0),
+              child: ListView.builder(
+                controller: _listViewController,
+                physics: const AlwaysScrollableScrollPhysics(),
+                itemCount: places.length,
+                itemBuilder: (context, index) {
+                  return TrailPlaceCard(
+                    key: ValueKey(places[index].id),
+                    place: places[index],
+                  );
+                },
+              ),
+            ),
+          ),
+        );
       },
     );
+  }
+
+  void _scrollToTop(newTab) {
+    if (newTab == 0 && _tabSelectionService.lastTapSame) {
+      _listViewController.animateTo(0.0,
+          duration:
+              Duration(milliseconds: _listViewController.position.pixels ~/ 2),
+          curve: Curves.easeOut);
+    }
   }
 
   @override
