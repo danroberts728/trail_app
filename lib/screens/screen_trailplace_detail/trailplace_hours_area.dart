74504163bcdbd1f301b196850859b3b877d28f6b
Dan Roberts <danroberts728@gmail.com>
2020-12-19 14:38:37 -0600

    Tap on hours title to expand hours

--- lib/screens/screen_trailplace_detail/trailplace_hours_area.dart
+++ lib/screens/screen_trailplace_detail/trailplace_hours_area.dart
@@ -3,7 +3,6 @@ import 'package:alabama_beer_trail/blocs/trailplace_hours_area_bloc.dart';
 import 'package:alabama_beer_trail/data/trail_place.dart';
 import 'package:alabama_beer_trail/util/open_hours_methods.dart';
 import 'package:alabama_beer_trail/util/trail_app_settings.dart';
-import 'package:alabama_beer_trail/widgets/expansion_section.dart';
 import 'package:flutter/material.dart';
 
 /// List of hours for the trail place screen
@@ -12,7 +11,8 @@ class TrailPlaceHoursArea extends StatefulWidget {
   final double iconSize;
   final double fontSize;
 
-  const TrailPlaceHoursArea({Key key, this.place, this.iconSize = 26.0, this.fontSize = 16.0})
+  const TrailPlaceHoursArea(
+      {Key key, this.place, this.iconSize = 26.0, this.fontSize = 16.0})
       : super(key: key);
 
   @override
@@ -21,6 +21,8 @@ class TrailPlaceHoursArea extends StatefulWidget {
 
 class _TrailPlaceHoursArea extends State<TrailPlaceHoursArea> {
   String _status;
+
+  bool _isPanelExpanded = false;
 
   TrailPlaceHoursAreaBloc _bloc = TrailPlaceHoursAreaBloc();
 
@@ -47,64 +49,94 @@ class _TrailPlaceHoursArea extends State<TrailPlaceHoursArea> {
 
   @override
   Widget build(BuildContext context) {
-    return ExpansionSection(
-      title: Text(
-        _status,
-        style: TextStyle(
-            fontSize: widget.fontSize,
-            color: OpenHoursMethods.isOpenToday(widget.place.hoursDetail, DateTime.now())
-                ? Colors.green
-                : Colors.black),
-      ),
-      leading: Icon(
-        Icons.access_time,
-        size: widget.iconSize,
-        color: TrailAppSettings.subHeadingColor,
-      ),
-      child: Container(
-        width: double.infinity,
-        padding: EdgeInsets.only(left: 35.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            'sunday',
-            'monday',
-            'tuesday',
-            'wednesday',
-            'thursday',
-            'friday',
-            'saturday'
-          ].map((key) {
-            return Row(
-              children: <Widget>[
-                Text(
-                  key[0].toUpperCase() + key.substring(1) + ": ",
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    height: 1.5,
-                    fontWeight: key == _nowDay
-                      ? FontWeight.bold
-                      : FontWeight.normal,
-                    color: Colors.black,
+    return ExpansionPanelList(
+      elevation: 0,
+      expandedHeaderPadding: EdgeInsets.zero,
+      animationDuration: Duration(seconds: 1),
+      expansionCallback: (panelIndex, isExpanded) {
+        setState(() {
+          _isPanelExpanded = !_isPanelExpanded;
+        });
+      },
+      children: [
+        ExpansionPanel(
+          canTapOnHeader: true,
+          isExpanded: _isPanelExpanded,
+          headerBuilder: (context, isExpanded) {
+            return Container(
+              padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 8.0),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Icon(
+                    Icons.access_time,
+                    size: widget.iconSize,
+                    color: TrailAppSettings.subHeadingColor,
                   ),
-                ),
-                Spacer(),
-                Text(
-                  widget.place.hours[key],
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    height: 1.5,
-                    fontWeight: key == _nowDay
-                      ? FontWeight.bold
-                      : FontWeight.normal,
-                    color: Colors.black,
+                  SizedBox(width: 4.0),
+                  Expanded(
+                    child: Text(
+                      _status,
+                      style: TextStyle(
+                        fontSize: widget.fontSize,
+                        color: OpenHoursMethods.isOpenToday(
+                                widget.place.hoursDetail, DateTime.now())
+                            ? Colors.green
+                            : Colors.black,
+                      ),
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             );
-          }).toList(),
+          },
+          body: Container(
+            width: double.infinity,
+            padding: EdgeInsets.symmetric(horizontal: widget.iconSize + 4.0, vertical: 0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                'sunday',
+                'monday',
+                'tuesday',
+                'wednesday',
+                'thursday',
+                'friday',
+                'saturday'
+              ].map((key) {
+                return Row(
+                  children: <Widget>[
+                    Text(
+                      key[0].toUpperCase() + key.substring(1) + ": ",
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        height: 1.5,
+                        fontWeight: key == _nowDay
+                            ? FontWeight.bold
+                            : FontWeight.normal,
+                        color: Colors.black,
+                      ),
+                    ),
+                    Spacer(),
+                    Text(
+                      widget.place.hours[key],
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        height: 1.5,
+                        fontWeight: key == _nowDay
+                            ? FontWeight.bold
+                            : FontWeight.normal,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                );
+              }).toList(),
+            ),
+          ),
         ),
-      ),
+      ],
     );
   }
 }
