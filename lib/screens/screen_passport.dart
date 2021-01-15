7b95bad8b15f8b791e4a1d9bc654b39f271f85c3
Dan Roberts <danroberts728@gmail.com>
2021-01-15 09:55:19 -0600

    Add scrollbar to passport

--- lib/screens/screen_passport.dart
+++ lib/screens/screen_passport.dart
@@ -10,121 +10,130 @@ import 'package:alabama_beer_trail/data/trail_database.dart';
 class ScreenPassport extends StatelessWidget {
   final ScreenPassportBloc _bloc = ScreenPassportBloc(TrailDatabase());
 
+  final ScrollController _scrollController = ScrollController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Passport"),
       ),
-      body: SingleChildScrollView(
-        child: StreamBuilder(
-          initialData: _bloc.stampInformation,
-          stream: _bloc.stream,
-          builder: (context, snapshot) {
-            List<StampInformation> stampedPlaces = snapshot.data;
-            if (stampedPlaces == null) {
-              return Container(
-                padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 16.0),
-                child: LinearProgressIndicator(),
-              );
-            } else if (stampedPlaces.isEmpty) {
-              return Container(
-                padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 16.0),
-                child: Text(
-                  "Nothing yet! Earn a stamp by checking into your first brewery.",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontStyle: FontStyle.italic,
+      body: Scrollbar(
+        controller: _scrollController,
+        isAlwaysShown: true,
+        child: SingleChildScrollView(
+          controller: _scrollController,
+          child: StreamBuilder(
+            initialData: _bloc.stampInformation,
+            stream: _bloc.stream,
+            builder: (context, snapshot) {
+              List<StampInformation> stampedPlaces = snapshot.data;
+              if (stampedPlaces == null) {
+                return Container(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 8.0, vertical: 16.0),
+                  child: LinearProgressIndicator(),
+                );
+              } else if (stampedPlaces.isEmpty) {
+                return Container(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 8.0, vertical: 16.0),
+                  child: Text(
+                    "Nothing yet! Earn a stamp by checking into your first brewery.",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontStyle: FontStyle.italic,
+                    ),
                   ),
-                ),
-              );
-            } else {
-              return LayoutBuilder(
-                builder: (context, constraints) {
-                  return Container(
-                    alignment: Alignment.centerLeft,
-                    width: double.infinity,
-                    padding: EdgeInsets.all(0),
-                    child: Column(
-                      children: [
-                        Wrap(
-                          direction: Axis.horizontal,
-                          alignment: WrapAlignment.start,
-                          runAlignment: WrapAlignment.start,
-                          runSpacing: 0,
-                          spacing: 0,
-                          children: stampedPlaces.map((s) {
-                            Random rand = Random(
-                                (s.stampDate != null
-                                    ? s.stampDate.millisecond
-                                    : 0));
-                            double boxSize = constraints.maxWidth / 3;
-                            double leftStampMargin =
-                                rand.nextDouble() * (boxSize - 75);
-                            double topStampMargin =
-                                rand.nextDouble() * (boxSize - 75);
-                            double tilt =
-                                ((rand.nextInt(30)) * rand.nextDouble()) * (rand.nextBool() ? 1 : -1);
-                            return Container(
-                              width: boxSize,
-                              height: boxSize,
-                              decoration: BoxDecoration(
-                                border:
-                                    Border.all(color: Colors.grey, width: 1.0),
-                              ),
-                              child: InkWell(
-                                onTap: () {
-                                  Feedback.forTap(context);
-                                  Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                          settings: RouteSettings(
-                                            name:
-                                                'Trail Place - ' + s.place.name,
-                                          ),
-                                          builder: (context) =>
-                                              TrailPlaceDetailScreen(
-                                                  place: s.place)));
-                                },
-                                child: Stack(
-                                  children: [
-                                    Container(
-                                      margin: EdgeInsets.all(32.0),
-                                      decoration: BoxDecoration(
-                                        image: DecorationImage(
-                                          colorFilter: ColorFilter.mode(
-                                              Colors.white.withOpacity(0.7),
-                                              BlendMode.modulate),
-                                          image: CachedNetworkImageProvider(
-                                            s.place.logoUrl,
+                );
+              } else {
+                return LayoutBuilder(
+                  builder: (context, constraints) {
+                    return Container(
+                      alignment: Alignment.centerLeft,
+                      width: double.infinity,
+                      padding: EdgeInsets.all(0),
+                      child: Column(
+                        children: [
+                          Wrap(
+                            direction: Axis.horizontal,
+                            alignment: WrapAlignment.start,
+                            runAlignment: WrapAlignment.start,
+                            runSpacing: 0,
+                            spacing: 0,
+                            children: stampedPlaces.map((s) {
+                              Random rand = Random((s.stampDate != null
+                                  ? s.stampDate.millisecond
+                                  : 0));
+                              double boxSize = constraints.maxWidth / 3;
+                              double leftStampMargin =
+                                  rand.nextDouble() * (boxSize - 75);
+                              double topStampMargin =
+                                  rand.nextDouble() * (boxSize - 75);
+                              double tilt =
+                                  ((rand.nextInt(30)) * rand.nextDouble()) *
+                                      (rand.nextBool() ? 1 : -1);
+                              return Container(
+                                width: boxSize,
+                                height: boxSize,
+                                decoration: BoxDecoration(
+                                  border: Border.all(
+                                      color: Colors.grey, width: 1.0),
+                                ),
+                                child: InkWell(
+                                  onTap: () {
+                                    Feedback.forTap(context);
+                                    Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                            settings: RouteSettings(
+                                              name: 'Trail Place - ' +
+                                                  s.place.name,
+                                            ),
+                                            builder: (context) =>
+                                                TrailPlaceDetailScreen(
+                                                    place: s.place)));
+                                  },
+                                  child: Stack(
+                                    children: [
+                                      Container(
+                                        margin: EdgeInsets.all(32.0),
+                                        decoration: BoxDecoration(
+                                          image: DecorationImage(
+                                            colorFilter: ColorFilter.mode(
+                                                Colors.white.withOpacity(0.7),
+                                                BlendMode.modulate),
+                                            image: CachedNetworkImageProvider(
+                                              s.place.logoUrl,
+                                            ),
                                           ),
                                         ),
                                       ),
-                                    ),
-                                    s.isStamped
-                                        ? Positioned(
-                                            top: topStampMargin,
-                                            left: leftStampMargin,
-                                            child: StampedPlaceIcon(
-                                                place: s.place,
-                                                firstCheckIn: s.stampDate,
-                                                tilt: tilt,
-                                                count: 1),
-                                          )
-                                        : Container(),
-                                  ],
+                                      s.isStamped
+                                          ? Positioned(
+                                              top: topStampMargin,
+                                              left: leftStampMargin,
+                                              child: StampedPlaceIcon(
+                                                  place: s.place,
+                                                  firstCheckIn: s.stampDate,
+                                                  tilt: tilt,
+                                                  count: 1),
+                                            )
+                                          : Container(),
+                                    ],
+                                  ),
                                 ),
-                              ),
-                            );
-                          }).toList(),
-                        ),
-                      ],
-                    ),
-                  );
-                },
-              );
-            }
-          },
+                              );
+                            }).toList(),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                );
+              }
+            },
+          ),
         ),
       ),
     );
